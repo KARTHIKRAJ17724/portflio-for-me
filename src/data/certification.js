1578ddb8bff36292fb6bf7b67ea5d2f3d3f440ea
Karthikraj <114292895+KARTHIKRAJ17724@users.noreply.github.com>
2025-02-04 00:21:14 +0530

    Add files via upload

---
+++ src/data/certification.js
@@ -0,0 +1,228 @@
+// certification.js
+import certImage1 from '../assets/images/certificates/1.jpg';
+import certImage2 from '../assets/images/certificates/2.jpg';
+import certImage3 from '../assets/images/certificates/3.jpg';
+import certImage4 from '../assets/images/certificates/4.jpg';
+import certImage5 from '../assets/images/certificates/5.jpg';
+import certImage6 from '../assets/images/certificates/6.jpg';
+import certImage7 from '../assets/images/certificates/7.jpg';
+import certImage8 from '../assets/images/certificates/8.jpg';
+import certImage9 from '../assets/images/certificates/9.jpg';
+import certImage10 from '../assets/images/certificates/10.jpg';
+import certImage11 from '../assets/images/certificates/11.jpg';
+import certImage12 from '../assets/images/certificates/12.jpg';
+import certImage13 from '../assets/images/certificates/13.jpg';
+import certImage14 from '../assets/images/certificates/14.jpg';
+import certImage15 from '../assets/images/certificates/15.jpg';
+import certImage16 from '../assets/images/certificates/16.jpg';
+import certImage17 from '../assets/images/certificates/17.jpg';
+import certImage18 from '../assets/images/certificates/18.jpg';
+import certImage19 from '../assets/images/certificates/19.jpg';
+import certImage20 from '../assets/images/certificates/20.jpg';
+import certImage21 from '../assets/images/certificates/21.jpg';
+import certImage22 from '../assets/images/certificates/22.jpg';
+import certImage23 from '../assets/images/certificates/23.jpg';
+import certImage24 from '../assets/images/certificates/24.jpg';
+import certImage25 from '../assets/images/certificates/25.jpg';
+import certImage26 from '../assets/images/certificates/26.jpg';
+import certImage27 from '../assets/images/certificates/27.jpg';
+
+
+export const certification = [
+  {
+    id: 28,
+    img: certImage28,
+    title: "Master Class for Machine learning",
+    desc: "",
+    link: certImage28,
+  },
+  {
+    id: 27,
+    img: certImage27,
+    title: "Master Class Data Analytics",
+    desc: "",
+    link: certImage27,
+  },
+  {
+    id: 26,
+    img: certImage26,
+    title: "ISP partner in Internshala",
+    desc: "",
+    link: certImage26,
+  },
+  {
+    id: 25,
+    img: certImage25,
+    title: "Technical Carear Counsling",
+    desc: "",
+    link: certImage25,
+  },
+  {
+    id: 24,
+    img: certImage24,
+    title: "Corprate Training Test",
+    desc: "",
+    link: certImage24,
+  },
+  {
+    id: 23,
+    img: certImage23,
+    title: "Web Development",
+    desc: "",
+    link: certImage23,
+  },
+  {
+    id: 22,
+    img: certImage22,
+    title: "Android Development",
+    desc: "",
+    link: certImage22,
+  },
+  {
+    id: 21,
+    img: certImage21,
+    title: "Python and AI",
+    desc: "",
+    link: certImage21,
+  },
+  {
+    id: 20,
+    img: certImage20,
+    title: "30 Days master class in DBMS",
+    desc: "",
+    link: certImage20,
+  },
+  {
+    id: 19,
+    img: certImage19,
+    title: "Certification for Appreciation",
+    desc: "",
+    link: certImage19,
+  },
+  {
+    id: 18,
+    img: certImage18,
+    title: "Hotstar Clone using HTML,CSS and Javascript",
+    desc: "",
+    link: certImage18,
+  },
+  {
+    id: 17,
+    img: certImage17,
+    title: "Graphic Designing",
+    desc: "",
+    link: certImage17,
+  },
+  {
+    id: 16,
+    img: certImage16,
+    title: "Learnothon",
+    desc: "",
+    link: certImage16,
+  },
+  {
+    id: 15,
+    img: certImage15,
+    title: "Dark Web Anonymity and Cyber security",
+    desc: "",
+    link: certImage15,
+  },
+  {
+    id: 14,
+    img: certImage14,
+    title: "Aws",
+    desc: "",
+    link: certImage14,
+  },
+  {
+    id: 13,
+    img: certImage13,
+    title: "E- Comerce quize",
+    desc: "",
+    link: certImage13,
+  },
+  {
+    id: 12,
+    img: certImage12,
+    title: "AI Tranfermative Learning",
+    desc: "",
+    link: certImage12,
+  },
+  {
+    id: 11,
+    img: certImage11,
+    title: "Data Anaytics Course",
+    desc: "",
+    link: certImage11,
+  },
+  {
+    id: 10,
+    img: certImage10,
+    title: "Data Science Course",
+    desc: "",
+    link: certImage10,
+  },
+  {
+    id: 9,
+    img: certImage9,
+    title: "10 Year Mann Baat",
+    desc: "",
+    link: certImage9,
+  },
+  {
+    id: 8,
+    img: certImage8,
+    title: "Data Science Course",
+    desc: "",
+    link: certImage8,
+  },
+  {
+    id: 7,
+    img: certImage7,
+    title: "Data Science Intership",
+    desc: "",
+    link: certImage7,
+  },
+  {
+    id: 6,
+    img: certImage6,
+    title: "Cyber Security Virtual Internship",
+    desc: "",
+    link: certImage6,
+  },
+  {
+    id: 5,
+    img: certImage5,
+    title: "Crafting Efective CV",
+    desc: "",
+    link: certImage5,
+  },
+  {
+    id: 4,
+    img: certImage4,
+    title: "Job interviwe Preperation",
+    desc: "",
+    link: certImage4,
+  },
+  {
+    id: 3,
+    img: certImage3,
+    title: "Git and Github Course",
+    desc: "",
+    link: certImage3,
+  },
+  {
+    id: 2,
+    img: certImage2,
+    title: "Android Development intern",
+    desc: "",
+    link: certImage2,
+  },
+  {
+    id: 1,
+    img: certImage1,
+    title: "AWS Academy Graduate - AWS Academy Cloud Architecting ",
+    desc: "",
+    link: certImage1,
+  },
+];
